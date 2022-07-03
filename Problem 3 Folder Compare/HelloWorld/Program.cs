edb0dccd01b20a363d0d2dad7d8cc01fd4d7bfb2
rifqi.nadzori <rifqi.nadzori@gmail.com>
2022-07-03 18:18:10 +0700

    edit 3

--- Problem 3 Folder Compare/HelloWorld/Program.cs
+++ Problem 3 Folder Compare/HelloWorld/Program.cs
@@ -10,25 +10,17 @@ namespace QueryCompareTwoDirs
         static void Main(string[] args)  
         {  
   
-            // Create two identical or different temporary folders
-            // on a local drive and change these file paths.  
             string pathA = @"C:\Users\user\Downloads\Direktori";  
             string pathB = @"C:\Users\user\Downloads\Direktori2";  
   
             System.IO.DirectoryInfo dir1 = new System.IO.DirectoryInfo(pathA);  
             System.IO.DirectoryInfo dir2 = new System.IO.DirectoryInfo(pathB);  
   
-            // Take a snapshot of the file system.  
             IEnumerable<System.IO.FileInfo> list1 = dir1.GetFiles("*.*", System.IO.SearchOption.AllDirectories);  
             IEnumerable<System.IO.FileInfo> list2 = dir2.GetFiles("*.*", System.IO.SearchOption.AllDirectories);  
-  
-            //A custom file comparer defined below  
+   
             FileCompare myFileCompare = new FileCompare();  
   
-            // This query determines whether the two folders contain  
-            // identical file lists, based on the custom file comparer  
-            // that is defined in the FileCompare class.  
-            // The query executes immediately because it returns a bool.  
             bool areIdentical = list1.SequenceEqual(list2, myFileCompare);  
   
             if (areIdentical == true)  
@@ -81,10 +73,7 @@ namespace QueryCompareTwoDirs
             Console.ReadKey();  
         }  
     }  
-  
-    // This implementation defines a very simple comparison  
-    // between two FileInfo objects. It only compares the name  
-    // of the files being compared and their length in bytes.  
+   
     class FileCompare : System.Collections.Generic.IEqualityComparer<System.IO.FileInfo>  
     {  
         public FileCompare() { }  
@@ -94,12 +83,7 @@ namespace QueryCompareTwoDirs
             return (f1.Name == f2.Name &&  
                     f1.Length == f2.Length);  
         }  
-  
-        // Return a hash that reflects the comparison criteria. According to the
-        // rules for IEqualityComparer<T>, if Equals is true, then the hash codes must  
-        // also be equal. Because equality as defined here is a simple value equality, not  
-        // reference identity, it is possible that two or more objects will produce the same  
-        // hash code.  
+    
         public int GetHashCode(System.IO.FileInfo fi)  
         {  
             string s = $"{fi.Name}{fi.Length}";
